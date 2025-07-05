ab49af2d005a4af11df50df2c8d34cc3dd9bae37
Bharath16-hub <Bharath.mbu.2k22@gmail.com>
2025-07-05 18:51:51 +0530

    Flow Control Statements

--- FCSQ14.java
+++ FCSQ14.java
@@ -1,10 +1,19 @@
 package java_fundamentals;
 
+import java.util.Scanner;
+
 public class FCSQ14 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int sum = 0;
+		while (a != 0) {
+			int rem = a % 10;
+			sum += rem;
+			a = a / 10;
+		}
+		System.out.println(sum);
 	}
 
 }
