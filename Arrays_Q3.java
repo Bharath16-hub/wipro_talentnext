0d39e726302956359bd97ababb53cf15246b5829
Bharath16-hub <Bharath.mbu.2k22@gmail.com>
2025-07-06 23:11:14 +0530

    Arrays

--- Arrays_Q3.java
+++ Arrays_Q3.java
@@ -2,20 +2,30 @@ package java_fundamentals;
 
 import java.util.*;
 
-public class Arrays_Q1 {
+public class Arrays_Q3 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+
 		int n = sc.nextInt();
+
 		int[] arr = new int[n];
-		int sum = 0;
+
 		for (int i = 0; i < n; i++) {
 			arr[i] = sc.nextInt();
-			sum += arr[i];
 		}
-		double average = (double) sum / n;
-		System.out.println("Sum = " + sum);
-		System.out.println("Average = " + average);
+
+		int search = sc.nextInt();
+
+		int index = -1;
+		for (int i = 0; i < n; i++) {
+			if (arr[i] == search) {
+				index = i;
+				break;
+			}
+		}
+
+		System.out.println(index);
 
 		sc.close();
 	}
