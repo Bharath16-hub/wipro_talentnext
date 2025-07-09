eeb5d32c219fcab6a2357855397284bc697405f6
Bharath16-hub <bharath.mbu.2k22@gmail.com>
2025-07-09 15:40:21 +0530

    java

---
+++ Question_4.java
@@ -0,0 +1,10 @@
+package oops_Inheritance;
+
+public class Question_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World");
+	}
+
+}
