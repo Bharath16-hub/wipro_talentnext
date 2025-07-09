f24a4b0f8157c1e3953687e85285798a8c33f2f4
Bharath16-hub <bharath.mbu.2k22@gmail.com>
2025-07-09 21:46:50 +0530

    9-July Inheritance

--- Question_4.java
+++ Question_4.java
@@ -1,10 +1,25 @@
 package oops_Inheritance;
 
-public class Question_4 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Hello World");
+class Animal2 {
+       void eat() {
+    	   System.out.println("eat");
+       }
+       void sleep() {
+    	   System.out.println("sleep");
+       }
+}
+class Bird extends Animal2{
+	void fly() {
+		System.out.println("i can fly");
 	}
-
+	
+}
+public class Question_4{
+	public static void main(String []args) {
+		Bird ob=new Bird();
+		
+		ob.eat();
+		ob.sleep();
+		ob.fly();
+	}
 }
